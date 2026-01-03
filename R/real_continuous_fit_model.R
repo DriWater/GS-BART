b1895e7c3cb24350c90089c763c7a2a87870c8f5
DriWater <heshuren987@gmail.com>
2026-01-03 14:22:51 -0600

    change code

--- R/real_continuous_fit_model.R
+++ R/real_continuous_fit_model.R
@@ -20,6 +20,39 @@ KingHouse_Time=difftime(Sys.time(), KingHouse_Time, units = "secs")
 KingHouse_MSPE = mean((KingHouse_GSB$yhat.test.mean - y.test.unstandardized)^2)
 KingHouse_MAPE = mean(abs(KingHouse_GSB$yhat.test.mean - y.test.unstandardized))
 
+load('data/KingHousePDP.Rdata')
+p1 <- ggplot(sf_bnd) + 
+  geom_sf(color = 'black', fill = NA, lwd = 0.75) + 
+  geom_sf(data = mesh_reg$mesh, aes(fill=y.test.unstandardized), color = NA, alpha =0.8) +
+  scale_fill_viridis_c(option = "H", direction = 1, name = 'log mean house price')+
+  theme_bw() + xlim(c(122.44,121.82)) + ylim(c(47.23,47.78))
+
+p2 <-ggplot(sf_bnd) + 
+  geom_sf(color = 'black', fill = NA, lwd = 0.8) + 
+  geom_point(data = coords_ho, aes(x = long, y = lat, color = group), inherit.aes = FALSE, size = 3) +
+  scale_colour_hue(guide = "none") + 
+  theme_bw() + theme(
+    axis.text.x = element_text(hjust = 0.5, size  = 7),
+    axis.text.y = element_text(hjust = 0.5, size  = 7),
+    plot.title = element_text(hjust = 0.5, size  = 15, face = "bold"),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank()) +
+  labs(x = "Longitude", y = "Latitude")
+
+
+p3 <- ggplot(data = df_new, aes(x = x, y = y, group = group, fill = group, color = group, ymin=CI_low, ymax=CI_high)) +
+  geom_line() + scale_colour_hue() + scale_fill_hue() + 
+  geom_ribbon(alpha=0.3, linewidth = 0) +
+  theme_bw() + theme(
+    legend.position="none",
+    axis.text.x = element_text(hjust = 0.5, size  = 7),
+    axis.text.y = element_text(hjust = 0.5, size  = 7),
+    plot.title = element_text(hjust = 0.5, size  = 15, face = "bold"),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank()) +
+  labs(x = "living area (square feet)", y = "Predicted log sale price")
+
+
 
 # US Election
 load("data/USelection.Rdata")
