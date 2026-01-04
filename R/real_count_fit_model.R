237fcf433654ec0f74d3c5f4245aa345b5e9425e
DriWater <heshuren987@gmail.com>
2026-01-04 14:22:52 -0600

    update datasets

--- R/real_count_fit_model.R
+++ R/real_count_fit_model.R
@@ -5,7 +5,7 @@ library(G2SBart)
 load("data/USCancer.Rdata")
 USCancer_Time = Sys.time()
 USCancer_Fit <- g2sbart(Y, Graphs, 200, 15, 200, "poisson", graphs_weight, hyperpar, modelpar, offset_train = 5*exp(offset_train), 
-                      offset_test = 5*exp(offset_test), nthreads = 1, verbose = T, seed = 1234)
+                      offset_test = 5*exp(offset_test), nthreads = 9, verbose = F, seed = 1234)
 USCancer_Time = difftime(Sys.time(), USCancer_Time, units = "secs")
 Y_pred_ho <- colMeans(t(t(exp(USCancer_Fit$phi.test))* 5 * exp(offset_test)))
 USCancer_RMSPE = sqrt(mean((Y_ho - Y_pred_ho)^2))
