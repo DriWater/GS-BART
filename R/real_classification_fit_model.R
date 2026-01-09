88fc89647455961c45a0c2bcd16a0da0e16e5ff3
DriWater <heshuren987@gmail.com>
2026-01-09 12:34:33 -0600

    Update code

--- R/real_classification_fit_model.R
+++ R/real_classification_fit_model.R
@@ -6,7 +6,7 @@ load("data/USFlood.Rdata")
 USFlood_Time = Sys.time()
 USFlood_Fit <- G2SBart::g2sbart(Y, Graphs, 200, 15, 200, family = "binary", graphs_weight,
                                 hyperpar = hyperpar, modelpar = modelpar,
-                                nthreads = 1, verbose = F, seed = 1234)
+                                nthreads = 9, verbose = F, seed = 1234)
 USFlood_Time = difftime(Sys.time(), USFlood_Time, units = "secs")
 Y_ho_pred <- as.numeric(USFlood_Fit$prob.test.mean > 0.5)
 USFlood_ACC = mean(Y_ho==Y_ho_pred)
@@ -15,7 +15,7 @@ USFlood_ACC = mean(Y_ho==Y_ho_pred)
 load("data/USAir.Rdata")
 USAir_Time = Sys.time()
 USAir_Fit <- G2SBart::g2sbart(Y_bin, Graphs, 200, 15, 200, family = "binary", graphs_weight,
-                              nthreads = 1, verbose = F, seed = 1234)
+                              nthreads = 9, verbose = F, seed = 1234)
 USAir_Time = difftime(Sys.time(), USAir_Time, units = "secs")
 Y_ho_pred <- as.numeric(USAir_Fit$prob.test.mean > 0.5)
 Y_bin_ho_tmp <- na.omit(Y_bin_ho)
--- R/real_count_fit_model.R
+++ R/real_count_fit_model.R
@@ -5,7 +5,7 @@ library(G2SBart)
 load("data/USCancer.Rdata")
 USCancer_Time = Sys.time()
 USCancer_Fit <- g2sbart(Y, Graphs, 200, 15, 200, "poisson", graphs_weight, hyperpar, modelpar, offset_train = 2*exp(offset_train), 
-                        offset_test = 2*exp(offset_test), nthreads = 1, verbose = T, seed = 1234)
+                        offset_test = 2*exp(offset_test), nthreads = 9, verbose = T, seed = 1234)
 USCancer_Time = difftime(Sys.time(), USCancer_Time, units = "secs")
 Y_pred_ho <- colMeans(t(t(exp(USCancer_Fit$phi.test))* 2 * exp(offset_test)))
 USCancer_RMSPE = sqrt(mean((Y_ho - Y_pred_ho)^2))
